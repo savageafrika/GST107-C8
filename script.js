aaef5bde19c971c25d4f796cac63d1292ad42656
savageafrika <113559289+savageafrika@users.noreply.github.com>
2023-11-20 07:10:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,203 @@
+
+
+
+
+ 
+
+   //Questions array
+
+   let questions = [
+
+    {
+
+         number:1,
+
+         question: "To make a good and comprehensive note one has to be",
+
+         answer : "A good listener",
+
+         options : [
+
+                          "A good listener",
+
+                          "A fast writer",
+
+                          "An effective thinker",
+
+                          "A well-rounded student"
+
+                        ]
+
+    
+
+    },
+
+    {
+
+         number:2,
+
+         question: "After listening to a teacher a student should be able to make",
+
+         answer : "Critical assessment",
+
+         options : [
+
+                          "Research",
+
+                          "New ideas",
+
+                          "Critical assessment",
+
+                          "Effective note"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:3,
+
+         question: "The purpose of note-taking does not include",
+
+         answer : "Record keeping",
+
+         options : [
+
+                          "Examination",
+
+                          "Record keeping",
+
+                          "Research",
+
+                          "Lectures"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:4,
+
+         question: "___ involves a rigorous and exhaustive academic enquiry",
+
+         answer : "Research",
+
+         options : [
+
+                          "Examination",
+
+                          "Record keeping",
+
+                          "Research",
+
+                          "Lectures"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:5,
+
+         question: "___ determines the effectiveness of our note taking",
+
+         answer : "Establishing a purpose",
+
+         options : [
+
+                          "Completeness",
+
+                          "Originality of our notes",
+
+                          "Relevance",
+
+                          "Establishing a purpose"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:6,
+
+         question: "Summary involves _&_ in writing",
+
+         answer : "Precision and concision",
+
+         options : [
+
+                          "Unity and coherence",
+
+                          "Precision and variety",
+
+                          "Logicality and validity",
+
+                          "Precision and concision"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:7,
+
+         question: "Which form of summary requires students to summarise a passage into a stipulated number of sentences",
+
+         answer : "Modern summary",
+
+         options : [
+
+                          "Modern summary",
+
+                          "Systematic summary",
+
+                          "Traditional summary",
+
+                          "Descriptive summary"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:8,
+
+         question: "Which of these is not a quality of summary",
+
+         answer : "Unity",
+
+         options : [
+
+                          "Brevity",
+
+                          "Clarity",
+
+                          "Unity",
+
+                          "Courage"  
+
+                          ]
+
+    
+
+    }
+
+     ];   
